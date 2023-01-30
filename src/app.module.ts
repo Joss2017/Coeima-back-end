356f04c3a080fd2b9cbc7c3468c2a8ced5570f52
Josselin Baptiste <josselin.baptiste@sncf.fr>
2023-01-30 12:03:57 +0100

    V1.1 générations des entités

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,37 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import * as dotenv from 'dotenv';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
+import { UsersModule } from './users/users.module';
+import { OffersModule } from './offers/offers.module';
+import { TopicsModule } from './topics/topics.module';
+import { CommentsModule } from './comments/comments.module';
+import { MessagesModule } from './messages/messages.module';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.local',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [User],
+      synchronize: process.env.MODE === 'DEV' ? true : false,
+    }),
+    UsersModule,
+    OffersModule,
+    TopicsModule,
+    CommentsModule,
+    MessagesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
